5f9a9baa127a6380fdf094623934d56d412df4a2
saulg-94 <94.gonzlezsaul@gmail.com>
2023-12-30 08:26:20 -0500

    updated getAllContent controller for fetch all resources; and tested it on thunder client; verified it works;

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,29 +1,41 @@
-import {StatusCodes} from 'http-status-codes'
-import ContentModel from '../models/ContentModel.js'
+import { StatusCodes } from "http-status-codes";
+import ContentModel from "../models/ContentModel.js";
 
 
+// GET ALL CONTENT
+export const getAllContent = async (req, res) => {
+  try {
+    const allContent = await ContentModel.find();
+    res.status(StatusCodes.OK).json({ status: "success", data: allContent });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ status: "fail", msg: err });
+  }
+};
 
-export const getAllContent = async(req,res)=>{
-    res.json({msg:'content/get-all-content api router controller endpoint'})
-}
 
-export const getSingleContent = async(req,res)=>{
-    res.json({msg:'content/get-single-content api router controller endpoint'})
-}
+// GET A SINGLE OBJECT FROM THE CONTENT DB COLLECTION
+export const getSingleContent = async (req, res) => {
+  res.json({
+    msg: "content/get-single-content api router controller endpoint",
+  });
+};
 
-export const updateContent = async(req,res)=>{
-    res.json({msg:'content/update-content api router controller endpoint'})
-}
 
-export const createContent = async(req,res)=>{
+// UPDATE A SINGLE OBJECT FROM TEH CONTENT DB COLLECTION
+export const updateContent = async (req, res) => {
+  res.json({ msg: "content/update-content api router controller endpoint" });
+};
 
-    try {
-        const newContent = await ContentModel.create(req.body);
 
-    res.status(StatusCodes.CREATED).json({status: 'success', data: newContent})
-    } catch (err) {
-        res.status(StatusCodes.BAD_REQUEST).json({status: 'fail', msg: err})
-    }
+// CREATE A NEW OBJECT PERSISTING IN CONTENT DB COLLECTION
+export const createContent = async (req, res) => {
+  try {
+    const newContent = await ContentModel.create(req.body);
 
-   
-}
+    res
+      .status(StatusCodes.CREATED)
+      .json({ status: "success", data: newContent });
+  } catch (err) {
+    res.status(StatusCodes.BAD_REQUEST).json({ status: "fail", msg: err });
+  }
+};
