523f2a4dc9d191e0664031bb228ac2bd30f1ebed
saulg-94 <94.gonzlezsaul@gmail.com>
2023-12-31 08:16:35 -0500

    updated the content MODEL by adding more fields/props to the model; for data rich api; WILL CHANGE OVER TIME

--- dummyCode.js
+++ dummyCode.js
@@ -73,6 +73,143 @@
 
 
 
+
+
+  // DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- 
+ const dataArray = [
+    {
+      "itemName": "Item 1",
+      "imageCover": "item1_cover.jpg",
+      "images": ["item1_image1.jpg", "item1_image2.jpg"],
+      "description": "Description for Item 1 goes here.",
+      "ingredients": ["Ingredient 1", "Ingredient 2"],
+      "price": 19.99,
+      "ratingsAverage": 4.2,
+      "ratingQuantity": 15,
+      "quantity": "In Stock",
+      "priceDiscount": 5,
+      "createdAt": "2023-01-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 2",
+      "imageCover": "item2_cover.jpg",
+      "images": ["item2_image1.jpg", "item2_image2.jpg"],
+      "description": "Detailed description for Item 2.",
+      "ingredients": ["Ingredient 3", "Ingredient 4"],
+      "price": 29.99,
+      "ratingsAverage": 4.7,
+      "ratingQuantity": 22,
+      "quantity": "Out of Stock",
+      "priceDiscount": null,
+      "createdAt": "2023-02-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 3",
+      "imageCover": "item3_cover.jpg",
+      "images": ["item3_image1.jpg", "item3_image2.jpg"],
+      "description": "Item 3 is a great choice.",
+      "ingredients": ["Ingredient 5", "Ingredient 6"],
+      "price": 39.99,
+      "ratingsAverage": 4.9,
+      "ratingQuantity": 30,
+      "quantity": "Limited Stock",
+      "priceDiscount": 8,
+      "createdAt": "2023-03-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 4",
+      "imageCover": "item4_cover.jpg",
+      "images": ["item4_image1.jpg", "item4_image2.jpg"],
+      "description": "The description for Item 4 is informative.",
+      "ingredients": ["Ingredient 7", "Ingredient 8"],
+      "price": 49.99,
+      "ratingsAverage": 4.5,
+      "ratingQuantity": 18,
+      "quantity": "In Stock",
+      "priceDiscount": null,
+      "createdAt": "2023-04-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 5",
+      "imageCover": "item5_cover.jpg",
+      "images": ["item5_image1.jpg", "item5_image2.jpg"],
+      "description": "A unique description for Item 5.",
+      "ingredients": ["Ingredient 9", "Ingredient 10"],
+      "price": 59.99,
+      "ratingsAverage": 4.3,
+      "ratingQuantity": 25,
+      "quantity": "In Stock",
+      "priceDiscount": 12,
+      "createdAt": "2023-05-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 6",
+      "imageCover": "item6_cover.jpg",
+      "images": ["item6_image1.jpg", "item6_image2.jpg"],
+      "description": "Item 6 is a must-have.",
+      "ingredients": ["Ingredient 11", "Ingredient 12"],
+      "price": 69.99,
+      "ratingsAverage": 4.8,
+      "ratingQuantity": 28,
+      "quantity": "Out of Stock",
+      "priceDiscount": 10,
+      "createdAt": "2023-06-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 7",
+      "imageCover": "item7_cover.jpg",
+      "images": ["item7_image1.jpg", "item7_image2.jpg"],
+      "description": "Description for Item 7.",
+      "ingredients": ["Ingredient 13", "Ingredient 14"],
+      "price": 79.99,
+      "ratingsAverage": 4.6,
+      "ratingQuantity": 20,
+      "quantity": "In Stock",
+      "priceDiscount": null,
+      "createdAt": "2023-07-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 8",
+      "imageCover": "item8_cover.jpg",
+      "images": ["item8_image1.jpg", "item8_image2.jpg"],
+      "description": "Detailed description for Item 8.",
+      "ingredients": ["Ingredient 15", "Ingredient 16"],
+      "price": 89.99,
+      "ratingsAverage": 4.4,
+      "ratingQuantity": 24,
+      "quantity": "Limited Stock",
+      "priceDiscount": 15,
+      "createdAt": "2023-08-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 9",
+      "imageCover": "item9_cover.jpg",
+      "images": ["item9_image1.jpg", "item9_image2.jpg"],
+      "description": "Item 9 is a top choice.",
+      "ingredients": ["Ingredient 17", "Ingredient 18"],
+      "price": 99.99,
+      "ratingsAverage": 4.7,
+      "ratingQuantity": 26,
+      "quantity": "In Stock",
+      "priceDiscount": null,
+      "createdAt": "2023-09-01T12:00:00.000Z"
+    },
+    {
+      "itemName": "Item 10",
+      "imageCover": "item10_cover.jpg",
+      "images": ["item10_image1.jpg", "item10_image2.jpg"],
+      "description": "Description for Item 10.",
+      "ingredients": ["Ingredient 19", "Ingredient 20"],
+      "price": 109.99,
+      "ratingsAverage": 4.9,
+      "ratingQuantity": 32,
+      "quantity": "Out of Stock",
+      "priceDiscount": 20,
+      "createdAt": "2023-10-01T12:00:00.000Z"
+    }
+  ]
+  // DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- DATA DUMP ---- 
+
  const usersArray = [
     {
       "name": "John",
--- models/ContentModel.js
+++ models/ContentModel.js
@@ -1,27 +1,33 @@
-import mongoose from 'mongoose';
-
-
+import mongoose from "mongoose";
 
 const ContentSchema = new mongoose.Schema({
-    productName: String,
-    image: String,
-    description: String,
-    price: {
-      type: Number,
-      default: 0.00,
-    },
-    quantity: {
-      type: String,
-      default: 'my city',
-    },
-    
-    
-  });
-  
-  ContentSchema.methods.toJSON = function () {
-    let obj = this.toObject();
-    delete obj.password;
-    return obj;
-  };
-  
-  export default mongoose.model('Content', ContentSchema);
+  itemName: { type: String, trim:true },
+  imageCover: String,
+  images: [String],
+  description: String,
+  extraField:[String],
+  price: {
+    type: Number,
+    default: 0.0,
+  },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingQuantity: { type: Number, default: 0 },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+ContentSchema.methods.toJSON = function () {
+  let obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export default mongoose.model("Content", ContentSchema);
