6c5d360d14713aa2f1b278cb19abf3dcd9df5a57
saulg-94 <94.gonzlezsaul@gmail.com>
2023-12-31 11:35:26 -0500

    created a script for importing and deleting data from the DB content COLLECTION; verified import-dev-data.js works;

---
+++ dev-data/import-dev-data.js
@@ -0,0 +1,62 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import fs from 'fs'
+import ContentModel from "../models/ContentModel.js";
+
+import mongoose from 'mongoose'
+
+
+
+
+const DB = process.env.MONGO_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+
+// SERVER CONNECTION TO DB
+const port = process.env.PORT || 4500;
+
+
+  mongoose.connect(DB).then(con => console.log('db connection successful'));
+  
+
+  
+
+
+
+
+// READ DATA
+
+const contents = JSON.parse(fs.readFileSync(`./dev-data/contents-data.json`,'utf-8'))
+
+
+// IMPORT DATA INTO DB
+
+const importData = async()=>{
+    try {
+        await ContentModel.create(contents);
+        console.log('Data Successfully loaded!');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+// DELETE ALL DATA IN DB
+
+const deleteData = async()=>{
+    try {
+        await ContentModel.deleteMany()
+        console.log('Data Successfully Deleted!');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+console.log(process.argv);
+
+if(process.argv[2]=== '--import'){
+    importData()
+} else if(process.argv[2]=== '--delete'){
+    deleteData()
+}
