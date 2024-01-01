c18aedc542f1768391b8d50c097c369dcf9e47bc
saulg-94 <94.gonzlezsaul@gmail.com>
2024-01-01 15:15:20 -0500

    created a apiFeatures.js file in /utils directory moved class APIFeatures and imported into contentController; verified it works

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import ContentModel from "../models/ContentModel.js";
-
+import APIFeatures from "../utils/apiFeatures.js";
 // TOP RATING ; SORTED BY PRICE
 export const aliasTopContent = async (req, res, next) => {
   req.query.limit = "5";
@@ -10,67 +10,6 @@ export const aliasTopContent = async (req, res, next) => {
   next();
 };
 
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query, this.queryString = queryString
-  }
-
-  filter() {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "limit", "sort", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    
-    console.log("success query log from getAllContent api endpoint");
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    
-    
-    this.query = this.query.find(JSON.parse(queryStr));
-    return this;
-    
-  }
-
-  sort() {
-    // 2) SORTING
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
-    return this;
-  }
-
-  limitFields(){
-
-    // 3) FIELD Limiting
-    if (this.queryString.fields) {
-        const fields = this.queryString.fields.split(",").join(" ");
-        this.query = this.query.select(fields);
-      } else {
-        this.query = this.query.select("-__v"); // this line excludes the string value is the field-name within mongoDB documents Model schemas'
-      }
-
-
-    return this;
-  }
-
-  paginate(){
-
-    // 4) Pagination
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-
-
-    return this;
-  }
-}
 
 // GET ALL CONTENT
 export const getAllContent = async (req, res) => {
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -2,3 +2,66 @@
 
 
 
+class APIFeatures {
+    constructor(query, queryString) {
+      this.query = query, this.queryString = queryString
+    }
+  
+    filter() {
+      // BUILD QUERY
+      // 1A) Filtering
+      const queryObj = { ...this.queryString };
+      const excludedFields = ["page", "limit", "sort", "fields"];
+      excludedFields.forEach((el) => delete queryObj[el]);
+      
+      console.log("success query log from getAllContent api endpoint");
+      // 1B) Advanced Filtering
+      let queryStr = JSON.stringify(queryObj);
+      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+      
+      
+      this.query = this.query.find(JSON.parse(queryStr));
+      return this;
+      
+    }
+  
+    sort() {
+      // 2) SORTING
+      if (this.queryString.sort) {
+        const sortBy = this.queryString.sort.split(",").join(" ");
+        this.query = this.query.sort(sortBy);
+      } else {
+        this.query = this.query.sort("-createdAt");
+      }
+      return this;
+    }
+  
+    limitFields(){
+  
+      // 3) FIELD Limiting
+      if (this.queryString.fields) {
+          const fields = this.queryString.fields.split(",").join(" ");
+          this.query = this.query.select(fields);
+        } else {
+          this.query = this.query.select("-__v"); // this line excludes the string value is the field-name within mongoDB documents Model schemas'
+        }
+  
+  
+      return this;
+    }
+  
+    paginate(){
+  
+      // 4) Pagination
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 100;
+      const skip = (page - 1) * limit;
+  
+      this.query = this.query.skip(skip).limit(limit);
+  
+  
+      return this;
+    }
+  }
+
+  export default APIFeatures
