0d9a3df4ef6f4ae2980aafca04bcd46563aefd3c
saulg-94 <94.gonzlezsaul@gmail.com>
2024-01-02 09:16:26 -0500

    added mongoDB DOCUMENT middleware to ContentModel.js; will be commented out; verified it works

--- models/ContentModel.js
+++ models/ContentModel.js
@@ -23,12 +23,43 @@ const ContentSchema = new mongoose.Schema({
     default: Date.now(),
     select: false,
   },
+},
+{
+  toJSON: { virtuals: true},
+  toObject: { virtuals: true}
 });
 
-ContentSchema.methods.toJSON = function () {
-  let obj = this.toObject();
-  delete obj.password;
-  return obj;
-};
+
+//Virtual properties should be utilized to seperate 'buisness logic'->(ContentModel.js) AWAY from 'application logic'->(contentController.js)
+//The result or value of this virtual field 'divideByTwo' can not be used by the database because it is not stored in DB
+ContentSchema.virtual('divideByTwo').get(function(){
+  return this.quantity / 2;
+});
+
+
+// [[MongoDB]] DOCUMENT MIDDLEWARE: runs before .save() and .create()
+ContentSchema.pre('save', function(next){
+  console.log('changes can be made here to the object/document OR things can happen here thru a chain of .pre and .post middleware REFERENCE MongoDB Documentation ');
+  console.log(this);
+  next()
+})
+
+ContentSchema.pre('save', function(next){
+  console.log('Will save document...');
+  next()
+})
+
+ContentSchema.post('save', function(doc,next){
+  console.log(doc);
+  console.log('this executes after the pre MIDDLEWARE');
+  next()
+})
+
+
+// ContentSchema.methods.toJSON = function () {
+//   let obj = this.toObject();
+//   delete obj.password;
+//   return obj;
+// };
 
 export default mongoose.model("Content", ContentSchema);
