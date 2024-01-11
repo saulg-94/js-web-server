7abbb880e783326ba34cef895f357763f4614a22
saulg-94 <94.gonzlezsaul@gmail.com>
2024-01-11 16:41:48 -0500

    added a custom validator to priceDiscount; validator checks if priceDiscount value is more than regular price; if so throw an validator error; next install a library for validation

--- models/ContentModel.js
+++ models/ContentModel.js
@@ -1,43 +1,69 @@
 import mongoose from "mongoose";
 
-const ContentSchema = new mongoose.Schema({
-  itemName: { type: String, trim:true, required:[true,'An item must have a name'],minlength:[3,'Must have more or equal to 3 characters'] },
-  imageCover: String,
-  images: [String],
-  description: String,
-  extraField:[String],
-  exampleField:{type: String,enum:{values:['option1','option2','option3'],message:'This example field allows for only a specified options',default:'option1'}},
-  price: {
-    type: Number,
-    default: 0.0,
-  },
-  ratingsAverage: { type: Number, default: 4.5,min:[1,'Rating must be above or equal 1'],max:[5,'Rating must be below or equal to 5'] },
-  ratingQuantity: { type: Number, default: 0 },
-  quantity: {
-    type: Number,
-    default: 0,
-  },
-  priceDiscount: Number,
+const ContentSchema = new mongoose.Schema(
+  {
+    itemName: {
+      type: String,
+      trim: true,
+      required: [true, "An item must have a name"],
+      minlength: [3, "Must have more or equal to 3 characters"],
+    },
+    imageCover: String,
+    images: [String],
+    description: String,
+    extraField: [String],
+    exampleField: {
+      type: String,
+      enum: {
+        values: ["option1", "option2", "option3"],
+        message: "This example field allows for only a specified options",
+        default: "option1",
+      },
+    },
+    price: {
+      type: Number,
+      default: 0.0,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above or equal 1"],
+      max: [5, "Rating must be below or equal to 5"],
+    },
+    ratingQuantity: { type: Number, default: 0 },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation 
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) should be below regular price",
+      },
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
   },
-},
-{
-  toJSON: { virtuals: true},
-  toObject: { virtuals: true}
-});
-
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 //Virtual properties should be utilized to seperate 'buisness logic'->(ContentModel.js) AWAY from 'application logic'->(contentController.js)
 //The field name of the result or value of this virtual field (in this case)->'divideByTwo' can not be used by the database because it is not stored in DB
 //But the value can be used in specific use cases (ex: displaying the results to UI for data anaylsis purposes)
-ContentSchema.virtual('divideByTwo').get(function(){
+ContentSchema.virtual("divideByTwo").get(function () {
   return this.quantity / 2;
 });
-
 
 // [[MongoDB]] DOCUMENT MIDDLEWARE: runs before .save() and .create()
 // ContentSchema.pre('save', function(next){
@@ -70,6 +96,5 @@ ContentSchema.virtual('divideByTwo').get(function(){
 //   console.log(this.pipeline());
 //   next()
 // })
-
 
 export default mongoose.model("Content", ContentSchema);
